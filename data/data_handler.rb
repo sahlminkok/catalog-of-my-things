100484b5276e48783dbd53388ed22e3cb528d50b
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-23 00:42:33 +0300

    Handle missing JSON file case scenario

--- data/data_handler.rb
+++ data/data_handler.rb
@@ -10,19 +10,24 @@ class DataHandler
     @data_manager.save_to_json('./data/books.json', books_data)
     puts 'Book saved to record successfully'
   rescue StandardError => e
-    puts "Error saving file to JSON: #{e.message}"
+    puts "Error loading from Json: #{e.message}"
+    puts 'The JSON file does not exist, returning empty array'
+    []
   end
 
   def load_books_from_json
-    books_data = @data_manager.load_from_json('./data/books.json')
+    file_path = './data/books.json'
+    if File.exist?(file_path)
+      books_data = @data_manager.load_from_json(file_path)
+    else
+      puts 'The JSON file does not exist. Creating empty file.'
+      File.write(file_path, '[]')
+      books_data = []
+    end
     loaded_books = []
     books_data.each do |book_data|
-      loaded_books << Book.new(
-        book_data[:publisher],
-        book_data[:cover_state],
-        book_data[:publish_date],
-        archived: book_data[:archived]
-      )
+      loaded_books << Book.new(book_data[:publisher], book_data[:cover_state],
+                               book_data[:publish_date], archived: book_data[:archived])
     end
     loaded_books
   rescue StandardError => e
