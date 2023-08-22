bcfd57f786190738a9c9ec1c6a40d8c7d56a9f5d
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-22 15:27:04 +0300

    Implement can_be_Archived method in Book class

--- book.rb
+++ book.rb
@@ -1,2 +1,14 @@
-class Book
+require_relative 'all_paths'
+
+class Book < Item
+  def initialize(publisher, cover_state)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?()
+    super() && @cover_state = 'bad'
+  end
 end
