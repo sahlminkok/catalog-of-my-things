e4bc6cb90220ce2f77ca41c339b2dbd62d6ce012
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-23 13:12:48 +0300

    Fix file paths

--- book.rb
+++ book.rb
@@ -1,4 +1,4 @@
-require_relative 'all_paths'
+require_relative 'item'
 
 class Book < Item
   attr_accessor :publisher, :cover_state
@@ -16,9 +16,7 @@ class Book < Item
     )
   end
 
-  private
-
   def can_be_archived?()
-    super || @cover_state = 'bad'
+    super || @cover_state == 'bad'
   end
 end
