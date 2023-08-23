a3932d71ed4d1acbaf67a5d7667d3324b9243a73
sahlminkok <sahlminkok7@gmail.com>
2023-08-23 17:49:19 +0100

    Create music album table

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,3 +6,11 @@ CREATE TABLE item (
   PRIMARY KEY (id),
   FOREIGN KEY (genre_id) REFERENCES genre(id)
 );
+
+CREATE TABLE music_album (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  publish_date DATE,
+  archived BOOLEAN,
+  on_spotify BOOLEAN,
+  PRIMARY KEY (id)
+);
