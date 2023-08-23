e3927585197b294f950217b26955f5d8f5e865c7
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-23 11:39:52 +0300

    Implement methods in game class

--- game.rb
+++ game.rb
@@ -1,4 +1,4 @@
-require_relative 'all_paths'
+require_relative 'item'
 
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
@@ -6,6 +6,13 @@ class Game < Item
   def initialize(multiplayer, last_played_at, publish_date, archived: false)
     super(publish_date, archived: archived)
     @multiplayer = multiplayer
-    @last_played_at
+    @last_played_at = last_played_at
+  end
+
+  private
+
+  def can_be_archived?()
+    duration = (Date.today.year - Date.parse(@last_played_at).year)
+    super && duration > 2
   end
 end
