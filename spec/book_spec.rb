421679d81549447b7faeab5760e96d74bf0624c7
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-23 13:00:47 +0300

    Fix linters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -11,18 +11,18 @@ RSpec.describe Book do
       expect(book.publish_date).to eq('2023-08-22')
       expect(book.archived).to be_falsey
     end
-      it "creates a new Book object with archived state" do
-      book = Book.new("Publisher Name", "bad", "2023-08-22", archived: true)
+    it 'creates a new Book object with archived state' do
+      book = Book.new('Publisher Name', 'bad', '2023-08-22', archived: true)
       expect(book.archived).to be_truthy
     end
   end
-  describe "#to_hash" do
-    it "returns a hash with the correct attributes" do
-      book = Book.new("Publisher Name", "good", "2023-08-22")
+  describe '#to_hash' do
+    it 'returns a hash with the correct attributes' do
+      book = Book.new('Publisher Name', 'good', '2023-08-22')
       hash = book.to_hash
-      expect(hash[:publisher]).to eq("Publisher Name")
-      expect(hash[:cover_state]).to eq("good")
-      expect(hash[:publish_date]).to eq("2023-08-22")
+      expect(hash[:publisher]).to eq('Publisher Name')
+      expect(hash[:cover_state]).to eq('good')
+      expect(hash[:publish_date]).to eq('2023-08-22')
       expect(hash[:archived]).to be_falsey
     end
   end
